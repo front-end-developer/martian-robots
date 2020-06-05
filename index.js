d232a34f9d0d58e16886b1a0ef4f66774dddb20d
front-end-developer <mark@adobeconsultant.co.uk>
2020-06-05 13:50:01 +0200

    first draft planning

---
+++ index.js
@@ -0,0 +1,41 @@
+
+// new grid x5 width, y5 height
+
+// grid class: contains maxWidth, maxHeight
+
+// grid class: contains method: grid.point(x, y)
+
+// grid class: contains method: grid.moveItem(Robot, Instruction: {})
+
+// grid class: contains method gridValidation() Max Length 50
+
+// grid class: contains method: grid.currentScent()
+
+// grid class: contains method: grid.getScents(), returns an array of sents
+
+// grid class: contains scents:[]
+
+// Optional
+// grid class: contains robots:[]
+
+// grid class OR Robot class: contains method: robot.output() RETURNS {X: d, y: d, compass: N|S|E|W, scent: true|false}
+
+
+
+
+// grid class: contains method:
+
+// new robot, x:1, y:1, orientation: E
+
+// robot class: contains method robotValidation() Max x = 50, Max y = 50
+
+// robot class: contains axis by orientation of degress, 0/360, 90 180, 270
+
+// robot class: contains compass: N, S, E, W perhaps as enums N: 'NORTH', S: 'SOUTH', E: 'EAST', W: 'WEST'
+
+// robot class: contains method instructionValidation() Max Length 100
+
+
+
+// robot class: contains position: x: digit, y: digit
+
